d44d8bfd765faca6942ddd0ee25caba148546660
virajbhoir <virajb1645@gmail.com>
2025-09-05 23:41:33 +0530

    changes

--- NakeYaFakeMobile/App.js
+++ NakeYaFakeMobile/App.js
@@ -1,18 +1,33 @@
+import React, { useState } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import Login from './src/screens/Login';
-import Dashboard from './src/screens/Dashboard';
+// 👇 Import screens from your mobile app folder
+import Login from "./src/screens/Login";
+import Dashboard from "./src/screens/Dashboard";
+import Result from "./src/screens/Result";
 
 const Stack = createStackNavigator();
 
 export default function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="Dashboard" component={Dashboard} />
+      <Stack.Navigator>
+        {!isLoggedIn ? (
+          // Show Login screen first
+          <Stack.Screen name="Login">
+            {(props) => <Login {...props} setIsLoggedIn={setIsLoggedIn} />}
+          </Stack.Screen>
+        ) : (
+          <>
+            <Stack.Screen name="Dashboard" component={Dashboard} />
+            <Stack.Screen name="Result" component={Result} />
+          </>
+        )}
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
+
